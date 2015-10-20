fb29b03c8114796f7534f3ada42aaaa3184cb652
Alex Macleod <alex@macleod.io>
2015-10-20 12:24:07 +0100

    Crash reporter: newline after the word at rather than every occurrence of the letters

--- app/src/main/java/me/ccrama/redditslide/Reddit.java
+++ app/src/main/java/me/ccrama/redditslide/Reddit.java
@@ -204,7 +204,7 @@ public class Reddit extends Application implements Application.ActivityLifecycle
                     t.printStackTrace(printWriter);
                     String s = writer.toString();
                     s = s.replace(";", ",");
-                    s = s.replace("at", "%0Aat");
+                    s = s.replace("at ", "%0Aat");
                     Log.v("Slide", "Slide crashed with " + s);
                     Intent i = new Intent(Reddit.this, Crash.class);
                     i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
