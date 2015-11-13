878c55b144106da1ed396f52f5234b8638352a20
ccrama <ccrama8@gmail.com>
2015-11-13 10:57:34 -0600

    Working on getting the coordinatorlayout working again

--- app/src/main/java/me/ccrama/redditslide/Notifications/NotificationJobScheduler.java
+++ app/src/main/java/me/ccrama/redditslide/Notifications/NotificationJobScheduler.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import me.ccrama.redditslide.Reddit;
 
@@ -26,6 +27,7 @@ public class NotificationJobScheduler   {
     public void start(Context c) {
         AlarmManager manager = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
         int interval = 1000 * 60 * Reddit.notificationTime;
+        Log.v("Slide", "NEW NOTIFICATIONS AT " + interval);
         manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
 
     }
