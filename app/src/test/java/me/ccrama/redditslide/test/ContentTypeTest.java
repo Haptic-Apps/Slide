f6cde2ff191cb402e4d3d2166ab6209989bfe258
l3d00m <thomasvanhalen01@gmail.com>
2016-01-28 19:32:53 +0100

    added test for OpenRedditLink.java
    refactored OpenRedditLink into smaller methods, makes it easier to read
    & to test

--- app/src/test/java/me/ccrama/redditslide/test/ContentTypeTest.java
+++ app/src/test/java/me/ccrama/redditslide/test/ContentTypeTest.java
@@ -13,37 +13,37 @@ import static org.junit.Assert.assertThat;
 public class ContentTypeTest {
 
     @Test
-    public void testContentTypeAlbum() {
+    public void testContentType_Album() {
         assertThat(ContentType.getImageType("http://www.imgur.com/a/duARTe"), is(ImageType.ALBUM));
         assertThat(ContentType.getImageType("https://imgur.com/gallery/DmXJ4"), is(ImageType.ALBUM));
     }
 
     @Test
-    public void testContentTypeGif() {
+    public void testContentType_Gif() {
         assertThat(ContentType.getImageType("https://i.imgur.com/33YIg0B.gifv"), is(ImageType.GIF));
         assertThat(ContentType.getImageType("https://i.imgur.com/33YIg0B.gif"), is(ImageType.GIF));
         assertThat(ContentType.getImageType("https://i.imgur.com/33YIg0B.gifnot"), is(not(ImageType.GIF)));
     }
 
     @Test
-    public void testContentTypeGfy() {
+    public void testContentType_Gfy() {
         assertThat(ContentType.getImageType("https://fat.gfycat.com/EcstaticLegitimateAnemone.webm"), is(ImageType.GFY));
         assertThat(ContentType.getImageType("https://thumbs.gfycat.com/EcstaticLegitimateAnemone-mobile.mp4"), is(ImageType.GFY));
     }
 
     @Test
-    public void testContentTypeImage() {
+    public void testContentType_Image() {
         assertThat(ContentType.getImageType("https://i.imgur.com/FGtUo6c.jpg"), is(ImageType.IMAGE));
         assertThat(ContentType.getImageType("https://i.imgur.com/FGtUo6c.png"), is(ImageType.IMAGE));
     }
 
     @Test
-    public void testContentTypeImgur() {
+    public void testContentType_Imgur() {
         assertThat(ContentType.getImageType("https://i.imgur.com/33YIg0B"), is(ImageType.IMGUR));
     }
 
     @Test
-    public void testContentTypeReddit() {
+    public void testContentType_Reddit() {
         assertThat(ContentType.getImageType("https://www.reddit.com/r/todayilearned/comments/42wgbg/til_the_tshirt_was_invented_in_1904_and_marketed/"), is(ImageType.REDDIT));
         assertThat(ContentType.getImageType("https://www.reddit.com/42wgbg/"), is(ImageType.REDDIT));
         assertThat(ContentType.getImageType("https://www.reddit.com/r/live/"), is(ImageType.REDDIT));
@@ -55,12 +55,12 @@ public class ContentTypeTest {
     }
 
     @Test
-    public void testContentTypeLink() {
+    public void testContentType_Link() {
         assertThat(ContentType.getImageType("https://stackoverflow.com/"), is(ImageType.LINK));
     }
 
     @Test
-    public void testContentTypeNone() {
+    public void testContentType_None() {
         assertThat(ContentType.getImageType(""), is(ImageType.NONE));
     }
 }
