9cf30c3f01c9e5c9388ea434d6cb41fb8f0d38cc
ccrama <ccrama8@gmail.com>
2016-02-16 17:56:54 -0600

    Made button to remove Synccit account, Extrated string resources

--- app/src/main/java/me/ccrama/redditslide/Activities/SettingsSynccit.java
+++ app/src/main/java/me/ccrama/redditslide/Activities/SettingsSynccit.java
@@ -40,11 +40,40 @@ public class SettingsSynccit extends BaseActivityAnim {
         name.setText(SettingValues.synccitName);
         auth.setText(SettingValues.synccitAuth);
 
+        if (SettingValues.synccitAuth.isEmpty()) {
+            (findViewById(R.id.remove)).setEnabled(false);
+        }
+        findViewById(R.id.remove).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (SettingValues.synccitAuth.isEmpty()) {
+
+                    new AlertDialogWrapper.Builder(SettingsSynccit.this)
+                            .setTitle(R.string.settings_synccit_delete)
+                            .setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    SettingValues.synccitName = "";
+                                    SettingValues.synccitAuth = "";
+                                    SharedPreferences.Editor e = SettingValues.prefs.edit();
+
+                                    e.putString(SettingValues.SYNCCIT_NAME, SettingValues.synccitName);
+                                    e.putString(SettingValues.SYNCCIT_AUTH, SettingValues.synccitAuth);
+                                    e.apply();
+                                    name.setText(SettingValues.synccitName);
+                                    auth.setText(SettingValues.synccitAuth);
+                                }
+                            }).setNegativeButton(R.string.btn_no, null)
+                            .show();
+                }
+            }
+        });
+
         findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Dialog d =  new MaterialDialog.Builder(SettingsSynccit.this)
-                        .title("Authenticating with Synccit")
+                final Dialog d = new MaterialDialog.Builder(SettingsSynccit.this)
+                        .title(R.string.settings_synccit_authenticate)
                         .progress(true, 100)
                         .cancelable(false)
                         .show();
@@ -68,10 +97,12 @@ public class SettingsSynccit extends BaseActivityAnim {
                                 e.putString(SettingValues.SYNCCIT_NAME, SettingValues.synccitName);
                                 e.putString(SettingValues.SYNCCIT_AUTH, SettingValues.synccitAuth);
                                 e.apply();
+                                (findViewById(R.id.remove)).setEnabled(true);
+
                                 new AlertDialogWrapper.Builder(SettingsSynccit.this)
-                                        .setTitle("Connected successfully!")
-                                        .setMessage("Synccit is now active for this device")
-                                        .setPositiveButton("Ok!", new DialogInterface.OnClickListener() {
+                                        .setTitle(R.string.settings_synccit_connected)
+                                        .setMessage(R.string.settings_synccit_active)
+                                        .setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
                                                 finish();
@@ -86,9 +117,9 @@ public class SettingsSynccit extends BaseActivityAnim {
                                 d.dismiss();
 
                                 new AlertDialogWrapper.Builder(SettingsSynccit.this)
-                                        .setTitle("Could not Authenticate")
-                                        .setMessage("Make sure your username and authentication key are correct!")
-                                        .setPositiveButton("Ok!", null).show();
+                                        .setTitle(R.string.settings_synccit_failed)
+                                        .setMessage(R.string.settings_synccit_failed_msg)
+                                        .setPositiveButton(R.string.btn_ok, null).show();
                             }
                         }
                     }, 3000);
@@ -97,9 +128,10 @@ public class SettingsSynccit extends BaseActivityAnim {
                     d.dismiss();
 
                     new AlertDialogWrapper.Builder(SettingsSynccit.this)
-                            .setTitle("Could not connect to Synccit servers")
-                            .setMessage("Make sure your username and authentication key are correct!")
-                            .setPositiveButton("Ok!", null).show();
+                            .setTitle(R.string.settings_synccit_failed)
+                            .setMessage(R.string.settings_synccit_failed_msg)
+                            .setPositiveButton(R.string.btn_ok, null).show();
+
                 }
 
 
