0de4086ac1356c8029792771b73da6c2d1bacf47
ccrama <ccrama8@gmail.com>
2016-02-28 11:48:30 -0600

    Added title to url when sharing a submission

--- app/src/main/java/me/ccrama/redditslide/Views/PopulateSubmissionViewHolder.java
+++ app/src/main/java/me/ccrama/redditslide/Views/PopulateSubmissionViewHolder.java
@@ -338,7 +338,7 @@ public class PopulateSubmissionViewHolder {
                                 mContext.startActivity(browserIntent);
                                 break;
                             case 4:
-                                Reddit.defaultShareText(submission.getUrl(), mContext);
+                                Reddit.defaultShareText(submission.getTitle() + " \n" + submission.getUrl(), mContext);
                                 break;
                             case 8:
                                 Reddit.defaultShareText(submission.getPermalink(), mContext);
