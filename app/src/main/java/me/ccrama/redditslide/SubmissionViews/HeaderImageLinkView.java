d3a295f4c39e23e49492bc4fecb3c81e029f31c2
ccrama <ccrama8@gmail.com>
2016-03-07 21:42:36 -0600

    Color fixes for bottom sheet

--- app/src/main/java/me/ccrama/redditslide/SubmissionViews/HeaderImageLinkView.java
+++ app/src/main/java/me/ccrama/redditslide/SubmissionViews/HeaderImageLinkView.java
@@ -7,6 +7,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v4.content.ContextCompat;
 import android.text.Html;
@@ -372,11 +376,25 @@ public class HeaderImageLinkView extends RelativeLayout {
                     activity = (Activity) ((android.support.v7.view.ContextThemeWrapper) getContext()).getBaseContext();
                 }
                 if (activity != null) {
-                    new BottomSheet.Builder(activity)
+                    BottomSheet.Builder b = new BottomSheet.Builder(activity)
                             .title(url)
-                            .grid()
-                            .sheet(R.menu.link_menu)
-                            .listener(new DialogInterface.OnClickListener() {
+                            .grid();
+                    int[] attrs = new int[]{R.attr.tint};
+                    TypedArray ta = getContext().obtainStyledAttributes(attrs);
+
+                    int color = ta.getColor(0, Color.WHITE);
+                    Drawable open = getResources().getDrawable(R.drawable.ic_open_in_browser);
+                    open.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+                    Drawable share = getResources().getDrawable(R.drawable.ic_share);
+                    share.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+                    Drawable copy = getResources().getDrawable(R.drawable.ic_content_copy);
+                    copy.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+
+                    b.sheet(R.id.open_link, open, getResources().getString(R.string.submission_link_extern));
+                    b.sheet(R.id.share_link, share, getResources().getString(R.string.share_link));
+                    b.sheet(R.id.copy_link, copy, getResources().getString(R.string.submission_link_copy));
+
+                    b.listener(new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     switch (which) {
