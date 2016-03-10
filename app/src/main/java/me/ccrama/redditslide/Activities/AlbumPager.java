af6315e8a92871cdc7b4dad7d2819b07a36e8c71
ccrama <ccrama8@gmail.com>
2016-03-10 16:39:32 -0600

    Made buttons in AlbumPager work for the image fragment

--- app/src/main/java/me/ccrama/redditslide/Activities/AlbumPager.java
+++ app/src/main/java/me/ccrama/redditslide/Activities/AlbumPager.java
@@ -1,15 +1,22 @@
 package me.ccrama.redditslide.Activities;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,20 +26,25 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.AlertDialogWrapper;
-import com.davemorrissey.labs.subscaleview.ImageSource;
-import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.afollestad.materialdialogs.folderselector.FolderChooserDialog;
 import com.google.gson.JsonElement;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.imageaware.ImageViewAware;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingProgressListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import it.sephiroth.android.library.tooltip.Tooltip;
 import me.ccrama.redditslide.ColorPreferences;
@@ -40,10 +52,13 @@ import me.ccrama.redditslide.ImageLoaderUtils;
 import me.ccrama.redditslide.R;
 import me.ccrama.redditslide.Reddit;
 import me.ccrama.redditslide.SettingValues;
+import me.ccrama.redditslide.Views.ImageSource;
 import me.ccrama.redditslide.Views.MediaVideoView;
+import me.ccrama.redditslide.Views.SubsamplingScaleImageView;
 import me.ccrama.redditslide.Views.ToolbarColorizeHelper;
 import me.ccrama.redditslide.util.AlbumUtils;
 import me.ccrama.redditslide.util.GifUtils;
+import me.ccrama.redditslide.util.LogUtil;
 import me.ccrama.redditslide.util.SubmissionParser;
 
 
@@ -54,7 +69,7 @@ import me.ccrama.redditslide.util.SubmissionParser;
  * instead of a RecyclerView (horizontal vs vertical). It also supports gifs and progress
  * bars which Album.java doesn't.
  */
-public class AlbumPager extends FullScreenActivity {
+public class AlbumPager extends FullScreenActivity implements FolderChooserDialog.FolderCallback {
     boolean gallery = false;
 
     @Override
@@ -349,6 +364,50 @@ public class AlbumPager extends FullScreenActivity {
 
             }
 
+            final String finalUrl  = url;
+            {
+                final ImageView iv = (ImageView) rootView.findViewById(R.id.share);
+                findViewById(R.id.external).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Reddit.defaultShare(finalUrl, AlbumPager.this);
+
+                    }
+                });
+                iv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showShareDialog(finalUrl);
+                    }
+                });
+                {
+                    final String finalUrl1 = url;
+                    findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v2) {
+
+
+                            try {
+                                ((Reddit) getApplication()).getImageLoader()
+                                        .loadImage(finalUrl, new SimpleImageLoadingListener() {
+                                            @Override
+                                            public void onLoadingComplete(String imageUri, View view, final Bitmap loadedImage) {
+                                                saveImageGallery(loadedImage, finalUrl1);
+                                            }
+
+                                        });
+                            } catch (Exception e) {
+                                Log.v(LogUtil.getTag(), "COULDN'T DOWNLOAD!");
+                            }
+
+                        }
+
+                    });
+                }
+
+
+            }
             final SubsamplingScaleImageView image = (SubsamplingScaleImageView) rootView.findViewById(R.id.image);
             ImageView fakeImage = new ImageView(getActivity());
             fakeImage.setLayoutParams(new LinearLayout.LayoutParams(image.getWidth(), image.getHeight()));
@@ -396,6 +455,186 @@ public class AlbumPager extends FullScreenActivity {
             Bundle bundle = this.getArguments();
             i = bundle.getInt("page", 0);
             user = images.get(i);
+        }
+    }
+    private void shareImage(String finalUrl) {
+        ((Reddit) getApplication()).getImageLoader()
+                .loadImage(finalUrl, new SimpleImageLoadingListener() {
+                    @Override
+                    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                        shareImage(loadedImage);
+                    }
+                });
+    }
+
+
+    private void saveImageGallery(final Bitmap bitmap, String URL) {
+        if (Reddit.appRestart.getString("imagelocation", "").isEmpty()) {
+            showFirstDialog();
+        } else if (!new File(Reddit.appRestart.getString("imagelocation", "")).exists()) {
+            showErrorDialog();
+        } else {
+            File f = new File(Reddit.appRestart.getString("imagelocation", "") + File.separator + UUID.randomUUID().toString() + ".png");
+
+
+            FileOutputStream out = null;
+            try {
+                f.createNewFile();
+                out = new FileOutputStream(f);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            } catch (Exception e) {
+                e.printStackTrace();
+                showErrorDialog();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                        showNotifPhoto(f.getAbsolutePath(), bitmap);
+
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    showErrorDialog();
+                }
+            }
+        }
+
+    }
+
+    public void showFirstDialog() {
+        new AlertDialogWrapper.Builder(this)
+                .setTitle("Set image save location")
+                .setMessage("Slide's image save location has not been set yet. Would you like to set this now?")
+                .setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new FolderChooserDialog.Builder(AlbumPager.this)
+                                .chooseButton(R.string.btn_select)  // changes label of the choose button
+                                .initialPath("/sdcard/")  // changes initial path, defaults to external storage directory
+                                .show();
+                    }
+                })
+                .setNegativeButton(R.string.btn_no, null)
+                .show();
+    }
+    public void showNotifPhoto(final String localAbsoluteFilePath, final Bitmap loadedImage) {
+        LogUtil.v(localAbsoluteFilePath);
+        MediaScannerConnection.scanFile(AlbumPager.this, new String[]{localAbsoluteFilePath}, null, new MediaScannerConnection.OnScanCompletedListener() {
+            public void onScanCompleted(String path, Uri uri) {
+
+                final Intent shareIntent = new Intent(Intent.ACTION_VIEW);
+                shareIntent.setDataAndType(Uri.parse(localAbsoluteFilePath), "image/*");
+                PendingIntent contentIntent = PendingIntent.getActivity(AlbumPager.this, 0, shareIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+
+                Notification notif = new NotificationCompat.Builder(AlbumPager.this)
+                        .setContentTitle(getString(R.string.info_photo_saved))
+                        .setSmallIcon(R.drawable.notif)
+                        .setLargeIcon(loadedImage)
+                        .setContentIntent(contentIntent)
+                        .setStyle(new NotificationCompat.BigPictureStyle()
+                                .bigPicture(loadedImage)).build();
+
+
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                mNotificationManager.notify(1, notif);
+                loadedImage.recycle();
+            }
+
+        });
+    }
+
+    private void shareImage(final Bitmap bitmap) {
+
+        if (Reddit.appRestart.getString("imagelocation", "").isEmpty()) {
+            showFirstDialog();
+        } else if (!new File(Reddit.appRestart.getString("imagelocation", "")).exists()) {
+            showErrorDialog();
+        } else {
+            File f = new File(Reddit.appRestart.getString("imagelocation", "") + File.separator + UUID.randomUUID().toString() + ".png");
+
+
+            FileOutputStream out = null;
+            try {
+                f.createNewFile();
+                out = new FileOutputStream(f);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            } catch (Exception e) {
+                e.printStackTrace();
+                showErrorDialog();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                        if ( !f.getAbsolutePath().isEmpty()) {
+                            Uri bmpUri = Uri.parse(f.getAbsolutePath());
+                            final Intent shareImageIntent = new Intent(android.content.Intent.ACTION_SEND);
+                            shareImageIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                            shareImageIntent.setType("image/png");
+                            startActivity(Intent.createChooser(shareImageIntent, getString(R.string.misc_img_share)));
+                        } else {
+                            showErrorDialog();
+                        }
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    showErrorDialog();
+                }
+            }
+        }
+
+
+    }
+    public void showErrorDialog() {
+        new AlertDialogWrapper.Builder(AlbumPager.this)
+                .setTitle("Uh oh, something went wrong.")
+                .setMessage("Slide couldn't save to the selected directory. Would you like to choose a new save location?")
+                .setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new FolderChooserDialog.Builder(AlbumPager.this)
+                                .chooseButton(R.string.btn_select)  // changes label of the choose button
+                                .initialPath("/sdcard/")  // changes initial path, defaults to external storage directory
+                                .show();
+                    }
+                })
+                .setNegativeButton(R.string.btn_no, null)
+                .show();
+    }
+    
+
+    private void showShareDialog(final String url) {
+        AlertDialogWrapper.Builder builder = new AlertDialogWrapper.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialoglayout = inflater.inflate(R.layout.sharemenu, null);
+
+        dialoglayout.findViewById(R.id.share_img).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareImage(url);
+            }
+        });
+
+        dialoglayout.findViewById(R.id.share_link).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Reddit.defaultShareText(url, AlbumPager.this);
+            }
+        });
+
+
+        builder.setView(dialoglayout);
+        builder.show();
+    }
+
+    @Override
+    public void onFolderSelection(FolderChooserDialog dialog, File folder) {
+        if (folder != null) {
+            Reddit.appRestart.edit().putString("imagelocation", folder.getAbsolutePath().toString()).apply();
+            Toast.makeText(this, "Images will be saved to " + folder.getAbsolutePath(), Toast.LENGTH_LONG).show();
         }
     }
 }
