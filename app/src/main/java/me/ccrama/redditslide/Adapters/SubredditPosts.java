db66d82adfedbaf262974f6e2ba6d48e160c756c
ccrama <ccrama8@gmail.com>
2016-03-21 23:24:11 -0500

    Fixed posts getting out of order if hiding while loading more data

--- app/src/main/java/me/ccrama/redditslide/Adapters/SubredditPosts.java
+++ app/src/main/java/me/ccrama/redditslide/Adapters/SubredditPosts.java
@@ -237,6 +237,7 @@ public class SubredditPosts implements PostLoader {
                     posts = new ArrayList<>(new LinkedHashSet(filteredSubmissions));
                     start = -1;
                 } else {
+                    posts = OfflineSubreddit.getSubreddit(subreddit).submissions;
                     posts.addAll(filteredSubmissions);
                     posts = new ArrayList<>(new LinkedHashSet(posts));
                     offline = false;
