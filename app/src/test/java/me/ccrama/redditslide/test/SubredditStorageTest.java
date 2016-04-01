61a020ac615d0bc4a61a2a8217109a0578f67b32
l3d00m <thomasvanhalen01@gmail.com>
2016-04-01 16:27:49 +0200

    fix failing travis, rename a string

--- app/src/test/java/me/ccrama/redditslide/test/SubredditStorageTest.java
+++ app/src/test/java/me/ccrama/redditslide/test/SubredditStorageTest.java
@@ -2,12 +2,9 @@ package me.ccrama.redditslide.test;
 
 import org.junit.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-import me.ccrama.redditslide.SubredditStorage;
 import me.ccrama.redditslide.UserSubscriptions;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,20 +16,20 @@ import static org.junit.Assert.assertThat;
  * Created by Alex Macleod on 28/03/2016.
  */
 public class SubredditStorageTest {
-    private ArrayList<String> subreddits = new ArrayList<String>(Arrays.asList(
+    private ArrayList<String> subreddits = new ArrayList<>(Arrays.asList(
             "xyy", "xyz", "frontpage", "mod", "friends", "random", "aaa"
     ));
 
     @Test
     public void sortsSubreddits() {
-        assertThat(UserSubscriptions.sort(subreddits), is(new ArrayList<String>(Arrays.asList(
+        assertThat(UserSubscriptions.sort(subreddits), is(new ArrayList<>(Arrays.asList(
                 "frontpage", "all", "random", "friends", "mod", "aaa", "xyy", "xyz"
         ))));
     }
 
     @Test
     public void sortsSubredditsNoExtras() {
-        assertThat(UserSubscriptions.sortNoExtras(subreddits), is(new ArrayList<String>(Arrays.asList(
+        assertThat(UserSubscriptions.sortNoExtras(subreddits), is(new ArrayList<>(Arrays.asList(
                 "frontpage", "random", "friends", "mod", "aaa", "xyy", "xyz"
         ))));
     }
