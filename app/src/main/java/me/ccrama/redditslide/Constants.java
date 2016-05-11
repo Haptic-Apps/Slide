2627c1516a1392b026ee98d07d31c892d4d6cbc8
nxt3 <nategantt@gmail.com>
2016-05-11 01:09:12 -0400

    Adjusted PTR for devices like the Priv

--- app/src/main/java/me/ccrama/redditslide/Constants.java
+++ app/src/main/java/me/ccrama/redditslide/Constants.java
@@ -1,20 +1,11 @@
 package me.ccrama.redditslide;
 
-import android.content.res.Resources;
-
 /**
  * Constants used throughout the app
  */
 public class Constants {
     // Maximum posts to request from Reddit
     public static final int PAGINATOR_POST_LIMIT = 25;
-
-    /**
-     * Height of the device's display, in px.
-     * This is used in conjunction with the multiplication of a percentage to adjust an offset.
-     * This is used when determining the offset for the SwipeRefreshLayout loading progress.
-     */
-    public static final int SCREEN_HEIGHT = Resources.getSystem().getDisplayMetrics().heightPixels;
 
     /**
      * This is the estimated height of the Tabs view mode in dp.
@@ -37,5 +28,5 @@ public class Constants {
      * This is used whenever we call mSwipeRefreshLayout.setProgressViewOffset().
      */
     public static final int PTR_OFFSET_TOP = Reddit.dpToPx(40);
-    public static final int PTR_OFFSET_BOTTOM = Reddit.dpToPx(18);
+    public static final int PTR_OFFSET_BOTTOM = Reddit.dpToPx(24);
 }
