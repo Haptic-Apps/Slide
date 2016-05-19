c06a8da6136fe5d6d2675eab6e1b53887150cdfc
ccrama <ccrama8@gmail.com>
2016-05-19 10:55:25 -0500

    Fixed small swipe back bug

--- app/src/main/java/me/ccrama/redditslide/Activities/MainActivity.java
+++ app/src/main/java/me/ccrama/redditslide/Activities/MainActivity.java
@@ -2753,7 +2753,7 @@ public class MainActivity extends BaseActivity {
 
         if (pager != null && commentPager) {
             if (pager.getCurrentItem() != toOpenComments && shouldLoad != null) {
-                if (usedArray != null && usedArray.indexOf(shouldLoad.contains("/m/") ? multiNameToSubsMap.get(shouldLoad) : shouldLoad) != pager.getCurrentItem())
+                if (usedArray != null && !shouldLoad.contains("+") && usedArray.indexOf(shouldLoad) != pager.getCurrentItem())
                     pager.setCurrentItem(toOpenComments - 1);
             }
         }
