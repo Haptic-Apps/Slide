0e8801cd7ee749c8075d79c4db0ec57a405e4b99
ccrama <ccrama8@gmail.com>
2016-09-03 12:48:04 -0500

    Added endless scrolling to Gallery mode

--- app/src/main/java/me/ccrama/redditslide/Activities/Gallery.java
+++ app/src/main/java/me/ccrama/redditslide/Activities/Gallery.java
@@ -35,12 +35,12 @@ import me.ccrama.redditslide.Views.CatchStaggeredGridLayoutManager;
  * Created by ccrama on 9/17/2015.
  */
 public class Gallery extends FullScreenActivity implements SubmissionDisplay {
-    public static final String EXTRA_PROFILE = "profile";
-    public static final String EXTRA_PAGE = "page";
-    public static final String EXTRA_SUBREDDIT = "subreddit";
+    public static final String EXTRA_PROFILE     = "profile";
+    public static final String EXTRA_PAGE        = "page";
+    public static final String EXTRA_SUBREDDIT   = "subreddit";
     public static final String EXTRA_MULTIREDDIT = "multireddit";
     public PostLoader subredditPosts;
-    public String subreddit;
+    public String     subreddit;
 
     public ArrayList<Submission> baseSubs;
 
@@ -57,15 +57,15 @@ public class Gallery extends FullScreenActivity implements SubmissionDisplay {
         }
         subreddit = multireddit == null ? subreddit : ("multi" + multireddit);
 
-        if(multireddit == null){
+        if (multireddit == null) {
             setShareUrl("https://reddit.com/r/" + subreddit);
         }
 
         applyDarkColorTheme(subreddit);
         super.onCreate(savedInstance);
         setContentView(R.layout.gallery);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        getWindow().getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -73,7 +73,8 @@ public class Gallery extends FullScreenActivity implements SubmissionDisplay {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
         long offline = getIntent().getLongExtra("offline", 0L);
 
-        final OfflineSubreddit submissions = OfflineSubreddit.getSubreddit(subreddit, offline, !Authentication.didOnline, this);
+        final OfflineSubreddit submissions =
+                OfflineSubreddit.getSubreddit(subreddit, offline, !Authentication.didOnline, this);
 
         baseSubs = new ArrayList<>();
 
@@ -88,37 +89,43 @@ public class Gallery extends FullScreenActivity implements SubmissionDisplay {
 
         rv = (RecyclerView) findViewById(R.id.content_view);
         recyclerAdapter = new GalleryView(this, baseSubs, subreddit);
-        RecyclerView.LayoutManager layoutManager = createLayoutManager(getNumColumns(getResources().getConfiguration().orientation));
+        RecyclerView.LayoutManager layoutManager =
+                createLayoutManager(getNumColumns(getResources().getConfiguration().orientation));
         rv.setLayoutManager(layoutManager);
         rv.setAdapter(recyclerAdapter);
-        /* Coming soon
         rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                if(subredditPosts instanceof SubredditPosts) {
-                    if (!((SubredditPosts)subredditPosts).loading) {
-                        visibleItemCount = rv.getLayoutManager().getChildCount();
-                        totalItemCount = rv.getLayoutManager().getItemCount();
-                        if ((visibleItemCount + pastVisiblesItems) + 5 >= totalItemCount) {
-                            ((SubredditPosts)subredditPosts).loading = true;
-                            ((SubredditPosts)subredditPosts).loadMore(Gallery.this, Gallery.this, false, subreddit);
-                        }
+                int[] firstVisibleItems;
+                firstVisibleItems =
+                        ((CatchStaggeredGridLayoutManager) rv.getLayoutManager()).findFirstVisibleItemPositions(
+                                null);
+                if (firstVisibleItems != null && firstVisibleItems.length > 0) {
+                    for (int firstVisibleItem : firstVisibleItems) {
+                        pastVisiblesItems = firstVisibleItem;
                     }
-                } else if(subredditPosts instanceof MultiredditPosts){
-                    if (!((MultiredditPosts)subredditPosts).loading) {
+                }
 
-                        visibleItemCount = rv.getLayoutManager().getChildCount();
-                        totalItemCount = rv.getLayoutManager().getItemCount();
-                        if ((visibleItemCount + pastVisiblesItems) + 5 >= totalItemCount) {
-                            ((MultiredditPosts)subredditPosts).loading = true;
+                if ((visibleItemCount + pastVisiblesItems) + 5 >= totalItemCount) {
+                    if (subredditPosts instanceof SubredditPosts) {
+                        if (!((SubredditPosts) subredditPosts).loading) {
+                            ((SubredditPosts) subredditPosts).loading = true;
+                            ((SubredditPosts) subredditPosts).loadMore(Gallery.this, Gallery.this,
+                                    false, subreddit);
+
+                        }
+                    } else if (subredditPosts instanceof MultiredditPosts) {
+                        if (!((MultiredditPosts) subredditPosts).loading) {
+                            ((MultiredditPosts) subredditPosts).loading = true;
                             (subredditPosts).loadMore(Gallery.this, Gallery.this, false);
+
                         }
                     }
                 }
             }
-            
-        });*/
+
+        });
 
     }
 
@@ -147,7 +154,9 @@ public class Gallery extends FullScreenActivity implements SubmissionDisplay {
             public void run() {
                 int startSize = baseSubs.size();
                 for (Submission s : submissions) {
-                    if (!baseSubs.contains(s) && s.getThumbnails() != null && s.getThumbnails().getSource() != null) {
+                    if (!baseSubs.contains(s)
+                            && s.getThumbnails() != null
+                            && s.getThumbnails().getSource() != null) {
                         baseSubs.add(s);
                     }
                 }
@@ -181,14 +190,16 @@ public class Gallery extends FullScreenActivity implements SubmissionDisplay {
 
     @NonNull
     private RecyclerView.LayoutManager createLayoutManager(final int numColumns) {
-        return new CatchStaggeredGridLayoutManager(numColumns, CatchStaggeredGridLayoutManager.VERTICAL);
+        return new CatchStaggeredGridLayoutManager(numColumns,
+                CatchStaggeredGridLayoutManager.VERTICAL);
     }
 
     private int getNumColumns(final int orientation) {
         final int numColumns;
         if (orientation == Configuration.ORIENTATION_LANDSCAPE && SettingValues.tabletUI) {
             numColumns = Reddit.dpWidth;
-        } else if (orientation == Configuration.ORIENTATION_PORTRAIT && SettingValues.dualPortrait) {
+        } else if (orientation == Configuration.ORIENTATION_PORTRAIT
+                && SettingValues.dualPortrait) {
             numColumns = 2;
         } else {
             numColumns = 1;
@@ -230,8 +241,21 @@ public class Gallery extends FullScreenActivity implements SubmissionDisplay {
                     Bundle args = new Bundle();
                     Submission submission = baseSubs.get(i);
                     String previewUrl = "";
-                    if (t != ContentType.Type.XKCD && submission.getDataNode().has("preview") && submission.getDataNode().get("preview").get("images").get(0).get("source").has("height")) { //Load the preview image which has probably already been cached in memory instead of the direct link
-                        previewUrl = submission.getDataNode().get("preview").get("images").get(0).get("source").get("url").asText();
+                    if (t != ContentType.Type.XKCD
+                            && submission.getDataNode().has("preview")
+                            && submission.getDataNode()
+                            .get("preview")
+                            .get("images")
+                            .get(0)
+                            .get("source")
+                            .has("height")) { //Load the preview image which has probably already been cached in memory instead of the direct link
+                        previewUrl = submission.getDataNode()
+                                .get("preview")
+                                .get("images")
+                                .get(0)
+                                .get("source")
+                                .get("url")
+                                .asText();
                     }
                     args.putString("contentUrl", submission.getUrl());
                     args.putString("firstUrl", previewUrl);
