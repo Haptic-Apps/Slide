50dab60163bb251f6cf77094fa7a25838b597640
John Seybold <jwseybold@gmail.com>
2016-11-28 11:29:01 -0500

    Immersive mode flag changes
    - Disable immersive mode when keyboard/menus are visible for a cleaner experience
    - Tested and works well on Pixel running stock Android 7.1

--- app/src/main/java/me/ccrama/redditslide/Activities/BaseActivity.java
+++ app/src/main/java/me/ccrama/redditslide/Activities/BaseActivity.java
@@ -64,9 +64,7 @@ public class BaseActivity extends PeekViewActivity
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             } else {
-                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             }
         }
     }
@@ -129,6 +127,10 @@ public class BaseActivity extends PeekViewActivity
                         @Override
                         public void onSystemUiVisibilityChange(int visibility) {
                             if ((visibility) == 0) {
+                                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                );
+                            } else {
                                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
