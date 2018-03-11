cff05ea07e1d6833e4ac51bd5fee73a4786dfd5d
James Downs <james.j.downs@gmail.com>
2018-03-11 11:34:17 -0400

    Updated gradle to 4.5 Replaced compile with implementation in dependencies Created a new product dimension for android P Fixed start up crashes affecting Android P

--- app/src/main/java/me/ccrama/redditslide/util/AdBlocker.java
+++ app/src/main/java/me/ccrama/redditslide/util/AdBlocker.java
@@ -1,15 +1,20 @@
 package me.ccrama.redditslide.util;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.WorkerThread;
 import android.webkit.WebResourceResponse;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
-import me.ccrama.redditslide.R;
+import okio.BufferedSource;
+import okio.Okio;
 
 /**
  * Created by Carlos on 8/12/2016.
@@ -17,16 +22,37 @@ import me.ccrama.redditslide.R;
  * Code adapted from http://www.hidroh.com/2016/05/19/hacking-up-ad-blocker-android/
  */
 public class AdBlocker {
-    private static HashSet<String> domains; //Use hash set for preformance
-    public static void init(Context context){
-        String allDomains = context.getString(R.string.domains);
-        domains = new HashSet<>();
-        domains.addAll(Arrays.asList(allDomains.split(",")));
+    private static final String      DOMAINS_FILE = "adblocksources.txt";
+    private static final Set<String> DOMAINS      = new HashSet<>(); //Use hash set for performance
+
+    public static void init(final Context context) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    loadFromAssets(context);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    @WorkerThread
+    private static void loadFromAssets(Context context) throws IOException {
+        InputStream stream = context.getAssets().open(DOMAINS_FILE);
+        BufferedSource buffer = Okio.buffer(Okio.source(stream));
+        String line;
+        while ((line = buffer.readUtf8Line()) != null) {
+            DOMAINS.add(line);
+        }
+        buffer.close();
+        stream.close();
     }
 
     public static boolean isAd(String url, Context context) {
-        if(domains == null)
-            init(context);
+        init(context);
         try {
             String host = new URL(url).getHost();
             return host != null && hostMatches(host);
@@ -40,7 +66,8 @@ public class AdBlocker {
         if(host.isEmpty())
             return false;
         int firstPeriod = host.indexOf(".");
-        return domains.contains(host) || firstPeriod + 1 < host.length() && domains.contains(host.substring(firstPeriod + 1));
+        return DOMAINS.contains(host) || firstPeriod + 1 < host.length() && DOMAINS.contains(
+                host.substring(firstPeriod + 1));
     }
 
     public static WebResourceResponse createEmptyResource() {
