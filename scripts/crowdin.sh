408b3baa7bff57c5fa2883d0549ee79fbabfdf03
Alex Macleod <alex@macleod.io>
2018-03-30 13:30:28 +0100

    Remove crowdin script wget dependency, check dependencies at start

--- scripts/crowdin.sh
+++ scripts/crowdin.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+depends() {
+    type $* >/dev/null || exit 1
+}
+
+depends git curl unzip
 
 basedir=$(git rev-parse --show-toplevel)
 apikey=$(tr -d ' \r\n' < "$basedir/scripts/crowdin.key")
@@ -6,12 +12,12 @@ apiurl="https://api.crowdin.com/api/project/slide-for-reddit"
 location='app/src/main/res'
 branch=${1:-application}
 
-if [ -z "$basedir" -o -z "$apikey" ]; then
+if [[ -z "$basedir" || -z "$apikey" ]]; then
     echo 'API key missing'
     exit 1
 fi
 
-if [ -n "$(git status --porcelain $basedir/$location)" ]; then
+if [[ -n "$(git status --porcelain $basedir/$location)" ]]; then
     echo 'Outstanding changes:'
     git status --short "$basedir/$location"
     exit 1
@@ -20,9 +26,9 @@ fi
 response=$(curl -sS "$apiurl/export?key=$apikey&branch=$branch" | grep '<success')
 echo $response
 
-if [ -n "$response" ]; then
+if [[ -n "$response" ]]; then
     tempfile=$(mktemp)
-    wget -qO "$tempfile" "$apiurl/download/all.zip?key=$apikey&branch=$branch"
+    curl -sSo "$tempfile" "$apiurl/download/all.zip?key=$apikey&branch=$branch"
     unzip -oqd "$basedir/$location" "$tempfile"
     rm "$tempfile"
     git --no-pager diff --stat --no-ext-diff "$basedir/$location"
