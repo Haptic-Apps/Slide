ac22a108e25e677623162b4cf35ed104e7a53c75
John Seybold <jwseybold@gmail.com>
2018-04-26 12:10:42 -0400

    [Bugfix] Fixed settings restore from file
    commit 30e3933b fixed this a while back in the noGPlay variant of this
    activity, but we should also fix this in the withGPlay version.

--- app/src/withGPlay/java/me.ccrama.redditslide/Activities/SettingsBackup.java
+++ app/src/withGPlay/java/me.ccrama.redditslide/Activities/SettingsBackup.java
@@ -36,6 +36,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -259,14 +260,14 @@ public class SettingsBackup extends BaseActivityAnim
                 Uri fileUri = data.getData();
                 Log.v(LogUtil.getTag(), "WORKED! " + fileUri.toString());
 
-                File path = new File(fileUri.getPath());
                 StringWriter fw = new StringWriter();
                 try {
-                    FileReader fr = new FileReader(path);
-                    int c = fr.read();
+                    InputStream is = getContentResolver().openInputStream(fileUri);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    int c = reader.read();
                     while (c != -1) {
                         fw.write(c);
-                        c = fr.read();
+                        c = reader.read();
                     }
                     String read = fw.toString();
                     if (read.contains("Slide_backupEND>")) {
