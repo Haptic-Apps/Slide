fdb3d60dac75f3955fb4785d8bd7d1bf8b50577c
James Downs <james.j.downs@gmail.com>
2018-06-24 22:53:26 -0400

    Added support for native spoiler markdown Added minimal tests for spoiler patterns Updated Mockito to 2.x

---
+++ app/src/test/java/me/ccrama/redditslide/test/SpoilerTextTest.java
@@ -0,0 +1,55 @@
+package me.ccrama.redditslide.test;
+
+import me.ccrama.redditslide.SpoilerRobotoTextView;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static junit.framework.Assert.fail;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({SpoilerRobotoTextView.class})
+public class SpoilerTextTest {
+    private final Pattern htmlSpoilerPattern = Whitebox.getInternalState(SpoilerRobotoTextView.class, "htmlSpoilerPattern");
+    private final Pattern nativeSpoilerPattern = Whitebox.getInternalState(SpoilerRobotoTextView.class, "nativeSpoilerPattern");
+
+    private final List<Object[]> htmlSpoilerTests = new ArrayList<Object[]>() {{
+        add(new Object[]{"<a href=\"#spoiler\">test</a>", true});
+        add(new Object[]{"<a href=\"#sp\">test</a>", true});
+        add(new Object[]{"<a href=\"#s\">test</a>", true});
+        add(new Object[]{"<a href=\"#not-a-spoiler\">test</a>", false});
+    }};
+
+    private final List<Object[]> nativeSpoilerTests = new ArrayList<Object[]>() {{
+        add(new Object[]{"<span class=\"md-spoiler-text\">test</span>", true});
+        add(new Object[]{"<span class=\"md-bold-text md-spoiler-text md-italic-text\">test</span>", true});
+        add(new Object[]{"<span class=\"not-a-spoiler\">test</span>", false});
+    }};
+
+    @Test
+    public void htmlSpoilerTest() {
+        spoilerTest(htmlSpoilerTests, htmlSpoilerPattern, "HTML spoiler test");
+    }
+
+    @Test
+    public void nativeSpoilerTest() {
+        spoilerTest(nativeSpoilerTests, nativeSpoilerPattern, "Native spoiler test");
+    }
+
+    private void spoilerTest(List<Object[]> tests, Pattern pattern, String name) {
+        for (Object[] test : tests) {
+            if (pattern.matcher((String) test[0]).matches() == (Boolean) test[1]) {
+                System.out.println(name.concat(": ").concat((String) test[0]).concat(" PASSED"));
+            } else {
+                System.out.println(name.concat(": ").concat((String) test[0]).concat(" FAILED"));
+                fail();
+            }
+        }
+    }
+}
