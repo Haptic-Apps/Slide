f247dda6befc79658f70c03abf97c63f2f403889
Rin <26451183+ahkuma@users.noreply.github.com>
2019-06-09 13:39:12 +0800

    refactor fullscreen activity

--- app/src/main/java/me/ccrama/redditslide/Activities/FullScreenActivity.java
+++ app/src/main/java/me/ccrama/redditslide/Activities/FullScreenActivity.java
@@ -1,13 +1,9 @@
 package me.ccrama.redditslide.Activities;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.Window;
 
-import jp.wasabeef.blurry.Blurry;
 import me.ccrama.redditslide.R;
 import me.ccrama.redditslide.Reddit;
 import me.ccrama.redditslide.Visuals.Palette;
@@ -20,37 +16,21 @@ import me.ccrama.redditslide.Visuals.Palette;
 public class FullScreenActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        //TODO something like this getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-             //   WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         if (Reddit.peek) {
             overridePendingTransition(R.anim.pop_in, 0);
         } else {
             overridePendingTransition(R.anim.slide_in, 0);
         }
+
         setRecentBar(null, Palette.getDefaultColor());
-
-
     }
+
     @Override
     public void finish() {
         super.finish();
         overridePendingTransition(0, R.anim.slide_out);
-    }
-
-    @Override
-    public void onPostCreate(Bundle savedInstanceState) {
-        try {
-            findViewById(android.R.id.content).getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-                @Override
-                public void onGlobalLayout() {
-                 //   Blurry.with(FullScreenActivity.this).radius(2).sampling(5).animate().color(Color.parseColor("#99000000")).onto((ViewGroup) findViewById(android.R.id.content));
-                }
-            });
-        } catch(Exception e){
-
-        }
-        super.onPostCreate(savedInstanceState);
     }
 }
