d9462e75506d853b722652d1e7d2cbb808722a8f
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-02-03 13:40:32 -0500

    Utilize empty implementation WebSocketAdapter

--- app/src/main/java/me/ccrama/redditslide/Activities/LiveThread.java
+++ app/src/main/java/me/ccrama/redditslide/Activities/LiveThread.java
@@ -32,13 +32,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.neovisionaries.ws.client.ThreadType;
 import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
 import com.neovisionaries.ws.client.WebSocketException;
 import com.neovisionaries.ws.client.WebSocketFactory;
-import com.neovisionaries.ws.client.WebSocketFrame;
-import com.neovisionaries.ws.client.WebSocketListener;
-import com.neovisionaries.ws.client.WebSocketState;
 
 import net.dean.jraw.managers.LiveThreadManager;
 import net.dean.jraw.models.LiveUpdate;
@@ -49,7 +46,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import me.ccrama.redditslide.Authentication;
 import me.ccrama.redditslide.ContentType;
@@ -203,85 +199,7 @@ public class LiveThread extends BaseActivityAnim {
 
                     try {
                         WebSocket ws = new WebSocketFactory().createSocket(thread.getWebsocketUrl());
-                        ws.addListener(new WebSocketListener() {
-                            @Override
-                            public void onStateChanged(
-                                    WebSocket websocket,
-                                    WebSocketState newState) {
-
-                            }
-
-                            @Override
-                            public void onConnected(
-                                    WebSocket websocket,
-                                    Map<String, List<String>> headers) {
-
-                            }
-
-                            @Override
-                            public void onConnectError(
-                                    WebSocket websocket,
-                                    WebSocketException cause) {
-
-                            }
-
-                            @Override
-                            public void onDisconnected(
-                                    WebSocket websocket,
-                                    WebSocketFrame serverCloseFrame,
-                                    WebSocketFrame clientCloseFrame, boolean closedByServer) {
-
-                            }
-
-                            @Override
-                            public void onFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onContinuationFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onTextFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onBinaryFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onCloseFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onPingFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onPongFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
+                        ws.addListener(new WebSocketAdapter() {
                             @Override
                             public void onTextMessage(
                                     WebSocket websocket, String s) {
@@ -321,125 +239,6 @@ public class LiveThread extends BaseActivityAnim {
                                     updates.remove(0);
                                     adapter.notifyItemRemoved(0);
                                 }*/
-
-                            }
-
-                            @Override
-                            public void onTextMessage(
-                                    WebSocket websocket, byte[] data) {
-
-                            }
-
-                            @Override
-                            public void onBinaryMessage(
-                                    WebSocket websocket,
-                                    byte[] binary) {
-
-                            }
-
-                            @Override
-                            public void onSendingFrame(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onFrameSent(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onFrameUnsent(
-                                    WebSocket websocket,
-                                    WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onThreadCreated(
-                                    WebSocket websocket,
-                                    ThreadType threadType, Thread thread) {
-
-                            }
-
-                            @Override
-                            public void onThreadStarted(
-                                    WebSocket websocket,
-                                    ThreadType threadType, Thread thread) {
-
-                            }
-
-                            @Override
-                            public void onThreadStopping(
-                                    WebSocket websocket,
-                                    ThreadType threadType, Thread thread) {
-
-                            }
-
-                            @Override
-                            public void onError(
-                                    WebSocket websocket,
-                                    WebSocketException cause) {
-
-                            }
-
-                            @Override
-                            public void onFrameError(
-                                    WebSocket websocket,
-                                    WebSocketException cause, WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onMessageError(
-                                    WebSocket websocket,
-                                    WebSocketException cause, List<WebSocketFrame> frames) {
-
-                            }
-
-                            @Override
-                            public void onMessageDecompressionError(
-                                    WebSocket websocket,
-                                    WebSocketException cause, byte[] compressed) {
-
-                            }
-
-                            @Override
-                            public void onTextMessageError(
-                                    WebSocket websocket,
-                                    WebSocketException cause, byte[] data) {
-
-                            }
-
-                            @Override
-                            public void onSendError(
-                                    WebSocket websocket,
-                                    WebSocketException cause, WebSocketFrame frame) {
-
-                            }
-
-                            @Override
-                            public void onUnexpectedError(
-                                    WebSocket websocket,
-                                    WebSocketException cause) {
-
-                            }
-
-                            @Override
-                            public void handleCallbackError(
-                                    WebSocket websocket,
-                                    Throwable cause) {
-
-                            }
-
-                            @Override
-                            public void onSendingHandshake(
-                                    WebSocket websocket,
-                                    String requestLine, List<String[]> headers) {
-
                             }
                         });
                         ws.connect();
