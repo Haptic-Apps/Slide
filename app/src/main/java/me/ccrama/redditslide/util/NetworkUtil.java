26ba73725c76bcfa4687c60158d97e48d05001e2
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-08-29 02:18:34 -0400

    Preliminary reformat of NetworkUtil

--- app/src/main/java/me/ccrama/redditslide/util/NetworkUtil.java
+++ app/src/main/java/me/ccrama/redditslide/util/NetworkUtil.java
@@ -24,20 +24,23 @@ public class NetworkUtil {
     /**
      * Uses the provided context to determine the current connectivity status.
      *
-     * @param context A context used to retrieve connection information from
-     * @return A non-null value defined in {@link Status}
+     * @param context A context used to retrieve connection information.
+     * @return A non-null value defined in {@link Status}.
      */
-    public static Status getConnectivityStatus(Context context) {
-        ConnectivityManager cm = ContextCompat.getSystemService(context, ConnectivityManager.class);
+    private static Status getConnectivityStatus(final Context context) {
+        final ConnectivityManager cm = ContextCompat.getSystemService(context, ConnectivityManager.class);
 
         assert cm != null;
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        final NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         switch (activeNetwork != null ? activeNetwork.getType() : CONST_NO_NETWORK) {
-            case ConnectivityManager.TYPE_WIFI: case ConnectivityManager.TYPE_ETHERNET:
+            case ConnectivityManager.TYPE_WIFI:
+            case ConnectivityManager.TYPE_ETHERNET:
                 if (cm.isActiveNetworkMetered())
                     return Status.MOBILE; // respect metered wifi networks as mobile
                 return Status.WIFI;
-            case ConnectivityManager.TYPE_MOBILE: case ConnectivityManager.TYPE_BLUETOOTH: case ConnectivityManager.TYPE_WIMAX:
+            case ConnectivityManager.TYPE_MOBILE:
+            case ConnectivityManager.TYPE_BLUETOOTH:
+            case ConnectivityManager.TYPE_WIMAX:
                 return Status.MOBILE;
             default:
                 return Status.NONE;
@@ -48,35 +51,44 @@ public class NetworkUtil {
      * Checks if the network is connected. An application context is said to have connection if
      * {@link #getConnectivityStatus(Context)} does not equal {@link Status#NONE}.
      *
-     * @param context The context used to retrieve connection information
-     * @return True if the application is connected, false if else.
+     * @param context The context used to retrieve connection information.
+     * @return true if the application is connected, false if otherwise.
      */
-    public static boolean isConnected(Context context) {
-        return !Reddit.appRestart.contains("forceoffline") && getConnectivityStatus(context) != Status.NONE;
+    public static boolean isConnected(final Context context) {
+        return !Reddit.appRestart.contains("forceoffline")
+                && isConnectedNoOverride(context);
     }
-    public static boolean isConnectedNoOverride(Context context) {
+
+    public static boolean isConnectedNoOverride(final Context context) {
         return getConnectivityStatus(context) != Status.NONE;
     }
+
     /**
      * Checks if the network is connected to WiFi.
      *
-     * @param context The context used to retrieve connection information
-     * @return True if the application is connected, false if else.
+     * @param context The context used to retrieve connection information.
+     * @return true if the application is connected, false if otherwise.
      */
     public static boolean isConnectedWifi(Context context) {
         return getConnectivityStatus(context) == Status.WIFI;
     }
+
     /**
-     * A simplified list of connectivity statuses. See {@link ConnectivityManager}'s {@code TYPE_*} for a full list.
-     *
-     * @author Matthew Dean
+     * A simplified list of connectivity statuses.
+     * See {@link ConnectivityManager}'s {@code TYPE_*} for a full list.
      */
-    public enum Status {
-        /** Operating on a wireless connection */
+    private enum Status {
+        /**
+         * Operating on a wireless connection.
+         */
         WIFI,
-        /** Operating on 3G, 4G, 4G LTE, etc. */
+        /**
+         * Operating on 3G, 4G, 4G LTE, etc.
+         */
         MOBILE,
-        /** No connection present */
+        /**
+         * No connection present.
+         */
         NONE
     }
 }
